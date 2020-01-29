c75db7bc1321d2c5a1d9630df86dbad2209d0dae
Zoey <zcolglazier@wisc.edu>
2020-01-29 15:22:01 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+//Final edits from Lesson 1
+//add the class 'foo' to the div
+  $('#mydiv').attr('class', 'foo');
+  $('#mydiv').css('color', 'purple');
+//initialize function called when the script loads
+function initialize(){
+    cities();
+};
+
+//function to create a table with cities and their populations
+function cities(){
+    //define two arrays for cities and population
+    var cityPop = [
+        {
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
+
+    //append the table element to the div
+    $("#mydiv").append("<table>");
+
+    //append a header row to the table
+    $("table").append("<tr>");
+
+    //add the "City" and "Population" columns to the header row
+    $("tr").append("<th>City</th><th>Population</th>");
+
+    //loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        $("table").append(rowHtml);
+    };
+    $('#mydiv').click(function(){
+      alert('This is hard.');
+    })
+};
+
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
